a1326d63f990162c0088a5f0893135b7d80a73f4
sk4r-git <thibaut.frin@gmail.com>
2026-02-10 15:03:34 +0100

    update

--- Utils/template.py
+++ Utils/template.py
@@ -1,37 +1,47 @@
 from pwn import *
 import subprocess
+import os
 from utils import *
 from file_struct import *
 
+CHALL_NAME = "./"
+final = 0
+debug = 0
 
-
+enc = lambda a: a.encode() if isinstance(a, str) else a
+sla = lambda a, b: io.sendlineafter(enc(a), enc(b))
+snl = lambda a: io.sendline(enc(a))
+sna = lambda a, b: io.sendafter(enc(a), enc(b))
+snd = lambda a: io.send(enc(a))
+rcu = lambda a: io.recvuntil(enc(a), drop=True)
+rcv = lambda a: io.recv(enc(a))
+rcl = lambda: io.recvline()
+p24 = lambda a: p32(a)[:-1]
+l64 = lambda a: u64(a.ljust(8, b"\x00"))
+l32 = lambda a: u64(a.ljust(4, b"\x00"))
+l16 = lambda a: u64(a.ljust(2, b"\x00"))
+lin = lambda a: log.info(f"{hex(a)=}")
+sen = lambda a: str(a).encode()
+mangle = lambda ptr, pos: ptr ^ (pos >> 12)
 
 def debug():
     pid = io.proc.pid
-    subprocess.run(["gnome-terminal", "--", "zsh", "-c", "gdb -nx -x ~/Desktop/Utils/custom_gdb.py -x g -p " + str(pid)])
-def sla(a, b):
-    res = io.recvuntil(a)
-    print("[+] RECEIVED :")
-    print(str(res))
-    print("[+] SEND :")
-    print(b)
-    io.sendline(b)
-def sl(a):
-    print("[+] SEND :")
-    print(a)
-    io.sendline(a)
-def rcu(a):
-    res = io.recvuntil(a)
-    print("[+] RECEIVED :")
-    print(res)
-    return res
+    subprocess.run(["gnome-terminal", "--", "zsh", "-c", "gdb -nx -x ./custom_gdb.py -x g -p " + str(pid)])
 
-#s1 = ssh(host=host, user=user, keyfile=key)
-io = process("")
-prog = Prog()
-prog.setpid(io.proc.pid)
-prog.getmap()
+libc_path = "./" + subprocess.run("ls | grep -E \"^libc\"", shell=True, capture_output=True).stdout.decode().strip()
+ld_path = "./" + subprocess.run("ls | grep -E \"^ld\"", shell=True, capture_output=True).stdout.decode().strip()
+
+exe = context.binary = ELF(CHALL_NAME, checksec=False)
+libc = ELF(libc_path, checksec=False)
+ld   = ELF(ld_path, checksec=False)
+
+if final == 1:
+    io = remote("", 0)
+else:
+    io = process(CHALL_NAME)
 
 
-debug()
-io.interactive()   
+
+if debug and not final:
+    debug()
+io.interactive()
--- init.sh
+++ init.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 
-rm -rf ./.git*
-rm gitp
+echo "sources are supposed to be already curled"
+echo "and under the following architecture :"
+echo "./pwn_xxx"
+echo "./pwn_yyy"
+echo "./re_xxx"
+echo "./re_yyy"
+echo "./cry_xxx"
+echo "./cry_yyy"
+
+echo "for pwn chall, dir contains either:"
+echo "  a zip file"
+echo "  a binary with libc"
+echo "  a binary with docker"
+
+sh=$SHELL
+sh2=$(basename $sh)
+pwd=$PWD
+rc="~/.${sh2}rc"
+echo "export PATH=\$PATH:$pwd/Utils" >> $rc
+# rm -rf ./.git*
+# rm gitp
