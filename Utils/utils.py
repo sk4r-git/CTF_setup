d4b9e15d332e0cdad2ef315c13403a2f2c3b0471
sk4r <sk4r@kali>
2026-03-14 08:56:26 +0100

    update

--- Utils/utils.py
+++ Utils/utils.py
@@ -30,7 +30,7 @@ def get_rand_n(n, m):
     cwd = os.getcwd()
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     rnt = process(["./rand_n", str(n), str(m+1)])
-    for i in range(n):
+    for i in range(m):
         rand_null_tab.append(int(rnt.recvuntil(b'\n')[:-1]))
     rnt.close()
     os.chdir(cwd)
